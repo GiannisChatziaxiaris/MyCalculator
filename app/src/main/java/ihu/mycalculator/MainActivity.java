b0ca8a2b5bc3e850fdd0288c31d04a935009c2a0
George Kouk <gkouklidis@hotmail.gr>
2023-10-29 22:19:00 +0200

    Δηλωσεις buttons

--- app/src/main/java/ihu/mycalculator/MainActivity.java
+++ app/src/main/java/ihu/mycalculator/MainActivity.java
@@ -3,13 +3,23 @@ package ihu.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+    MaterialButton button_plus,button_minus,button_multiply,button_divide;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_plus=findViewById(R.id.button_plus);
+        button_divide=findViewById(R.id.button_divide);
+        button_multiply=findViewById(R.id.button_multiply);
+        button_minus=findViewById(R.id.button_minus);
     }
 }
-//test allagh -bibos
